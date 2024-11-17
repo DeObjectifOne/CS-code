5b55337ed4a3a81c10ee4a85813e1a87865909dc
DeObjectifOne <ethan.tongson1806@gmail.com>
2024-11-17 21:02:43 +0000

    Update __init__.py

--- Coding time!/website/__init__.py
+++ Coding time!/website/__init__.py
@@ -1,7 +1,12 @@
 from flask import Flask
+
 from flask_sqlalchemy import SQLAlchemy
+
 import secrets
+
 import os
+
+from flask_login import LoginManager
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
@@ -17,9 +22,18 @@ def create_app():
     from .views import views
     app.register_blueprint(views)
 
+    login_manager = LoginManager()
+    login_manager.login_view = 'views.login'
+    login_manager.init_app(app)
+
     # Import models so tables can be created
     from .models import User, Task
     with app.app_context():
         db.create_all()
 
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+    
+    
     return app
