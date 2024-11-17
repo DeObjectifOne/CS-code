757e621a211465855a2e31fa061262085488ee6a
DeObjectifOne <ethan.tongson1806@gmail.com>
2024-11-17 21:03:23 +0000

    Update views.py

--- Coding time!/website/views.py
+++ Coding time!/website/views.py
@@ -20,4 +20,41 @@ def login():
 #route for the register page
 @views.route('/register')
 def register():
-    return render_template('register.html')
+    
+  if current_user.is_authenticated:
+        flash('You are already logged in', category='info')
+        return redirect(url_for('views.home'))
+
+    if request.method == "POST":
+        username = request.form.get("username")
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        if not username or not email or not password:
+            flash('All fields are required', category='error')
+            return redirect(url_for('views.register'))
+
+        user = User.query.filter_by(email=email).first()
+
+        if user:
+            flash('A user with this email already exists', category='error')
+            return redirect(url_for('views.login'))
+        else:
+            try:
+                new_user = User(
+                    username=username, 
+                    email=email, 
+                    password=generate_password_hash(password, method='pbkdf2:sha256')
+                )
+                db.session.add(new_user)
+                db.session.commit()
+                login_user(new_user, remember=True)
+                flash('Registration successful')
+                return redirect(url_for('views.home'))
+            except Exception as e:
+                db.session.rollback()
+                flash('An error occured while trying you register you, please try again')
+                print(f"Error: {e}")
+
+
+    return render_template('register.html', user=current_user)
