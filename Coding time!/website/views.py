56386f8f6c62de3499af0b2fae9292e0db3cc4f7
DeObjectifOne <ethan.tongson1806@gmail.com>
2024-11-17 21:13:39 +0000

    Update views.py

--- Coding time!/website/views.py
+++ Coding time!/website/views.py
@@ -1,5 +1,19 @@
-from flask import Blueprint, render_template
+#import flask modules for route handling and get and post requests
+from flask import Blueprint, render_template, request, flash, redirect, url_for
 
+#imports the database for data handling
+from . import db
+
+#import the user table to check for pre-existing users
+from .models import User
+
+#used to handle user data depending on their action
+from flask_login import login_user, login_required, logout_user, current_user
+
+#used to make a WSGI (Web Server Gateway Interface) to handle user security
+from werkzeug.security import check_password_hash, generate_password_hash
+
+#blueprint for the views template
 views = Blueprint('views', __name__)
 
 #route for the home page
@@ -20,20 +34,26 @@ def login():
 #route for the register page
 @views.route('/register')
 def register():
-    
-  if current_user.is_authenticated:
+
+    #function that prevents the user from registering
+    #detects this by seeing if the user is authenticated
+    if current_user.is_authenticated:
         flash('You are already logged in', category='info')
         return redirect(url_for('views.home'))
 
+    #main registration function
+    #retrieves user details
     if request.method == "POST":
         username = request.form.get("username")
         email = request.form.get("email")
         password = request.form.get("password")
 
+        #function to make sure all fields are complete
         if not username or not email or not password:
             flash('All fields are required', category='error')
             return redirect(url_for('views.register'))
 
+        #the user's details are checked to see if a duplicate email exists
         user = User.query.filter_by(email=email).first()
 
         if user:
@@ -41,16 +61,21 @@ def register():
             return redirect(url_for('views.login'))
         else:
             try:
+                #the user is created as a variable with 3 attributes
                 new_user = User(
                     username=username, 
                     email=email, 
+                    #the password is hashed for increased security
                     password=generate_password_hash(password, method='pbkdf2:sha256')
                 )
+                #the user is then added to the database
                 db.session.add(new_user)
                 db.session.commit()
+                #the application then remebers the user's login
                 login_user(new_user, remember=True)
                 flash('Registration successful')
                 return redirect(url_for('views.home'))
+            #function for if any errors occur during the process
             except Exception as e:
                 db.session.rollback()
                 flash('An error occured while trying you register you, please try again')
