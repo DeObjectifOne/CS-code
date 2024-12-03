caed5438e8859074f2840a3cecdb424c2cd9e836
DeObjectifOne <ethan.tongson1806@gmail.com>
2024-12-03 09:21:54 +0000

    Update __init__.py

--- Coding time!/website/__init__.py
+++ Coding time!/website/__init__.py
@@ -19,18 +19,25 @@ DB_NAME = "database.db"
 
 #app function that instantiates all connected roots
 def create_app():
-    
+
+    #changes the function name
+    #makes it shorter to type out
     app = Flask(__name__)
+    #used for website encryption
+    #added layer of security
     app.secret_key = secrets.token_hex(24)
+    #the database is set to run once the website runs
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
 
-    # Import and register blueprint
+    # Import and register blueprints
+    #this is so they're run when the website runs
     from .auth import auth
     from .views import views
     from .utils import utils
     from .sort import sort
 
+    #makes the url link for these blueprints valid
     app.register_blueprint(auth, url_prefix='/')
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(utils, url_prefix='/')
@@ -47,16 +54,18 @@ def create_app():
         db.create_all()
 
     #used to return the user's details by using their id
+    #this also works for linking the user to their tasks
     @login_manager.user_loader
     def load_user(id):
         return User.query.get(int(id))
 
-    #used to insert the user each time they log into the website
+    #used to insert any of the dictionaries the user requires
     @app.context_processor
     def inject_user():
         return dict(user=current_user)
 
     #used to make sure that any datetime objects are formatted correctly
+    #this allows for better display and editing of the task
     @app.template_filter('datetimeformat')
     def datetimeformat(value):
         if value:
