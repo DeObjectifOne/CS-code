08225510f7392604c9fdcd7f7bf54e91d69d4bda
DeObjectifOne <ethan.tongson1806@gmail.com>
2024-12-10 10:14:36 +0000

    Update config.py

--- Coding time!/config.py
+++ Coding time!/config.py
@@ -1,6 +1,13 @@
+#imports the whole app into this page
+#this is so it can all be run at once
 from website import create_app
 
+#function is established
 app = create_app()
 
+#the code is set to debugging mode
+#this is for development purposes
+#as changes can be made live
+#without having to restart the application
 if __name__ == '__main__':
     app.run(debug=True)
