e01bfe767f48d926b3bfde83c80064b9089401cf
DeObjectifOne <ethan.tongson1806@gmail.com>
2025-03-08 16:44:10 +0000

    Update models.py

--- Coding time!/website/models.py
+++ Coding time!/website/models.py
@@ -5,8 +5,6 @@ from datetime import datetime
 #UserMixin is able to call up the user properties
 #this is used for things like authentication
 from flask_login import UserMixin
-#imports sqlalchemy so the tables can be authenticated
-from sqlachemy.sql import func
 
 #user table
 class User(db.Model, UserMixin):
@@ -47,6 +45,8 @@ class Task(db.Model):
     #variables used for task filtering
     completed = db.Column(db.Boolean, default=False)
     starred = db.Column(db.Boolean, default=False)
+    #position value so the database knows where the task is in the lisy
+    position = db.Column(db.Integer, default=0)
     #variable used to link all tasks back to the required user
     #a ForeignKey is used so the Task class is directly linked back to the User class
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
@@ -58,16 +58,10 @@ class Preferences(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     #stores the saved weights of 4 variables
     #these variables are used so the user can automatically sort their tasks
-    weight_due_date = db.Column(db.Float, nullable=False, default=0.4)
-    weight_duration = db.Column(db.Float, nullable=False, default=0.3)
-    weight_priority = db.Column(db.Float, nullable=False, default=0.2)
-    weight_difficulty = db.Column(db.Float, nullable=False, default=0.1)
-    #stores the value of the enabled 4 variables
-    #this allows the user to turn on and off some variables
-    enable_due_date = db.Column(db.Boolean, default=True)
-    enable_duration = db.Column(db.Boolean, default=True)
-    enable_priority = db.Column(db.Boolean, default=True)
-    enable_difficulty = db.Column(db.Boolean, default=True)
+    due_date_weight = db.Column(db.Float, nullable=False, default=0.25)
+    duration_weight = db.Column(db.Float, nullable=False, default=0.25)
+    priority_weight = db.Column(db.Float, nullable=False, default=0.25)
+    difficulty_weight = db.Column(db.Float, nullable=False, default=0.25)
     #variable used to link all the user's preferences back to the required user
     #a ForeignKey is used so the Preferences class is directly linked back to the User class
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
